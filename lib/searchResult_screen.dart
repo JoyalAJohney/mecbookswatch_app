7454c1932ebfad444724effddbe710c3da1930c8
JoyalAJohney <joyalajohny@gmail.com>
2018-06-05 15:22:12 +0530

    Retrieve JSON via HTTP Get

--- lib/searchResult_screen.dart
+++ lib/searchResult_screen.dart
@@ -1,5 +1,6 @@
+import 'dart:async';
 import 'dart:convert';
-
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
 class SearchResult extends StatefulWidget {
@@ -8,44 +9,63 @@ class SearchResult extends StatefulWidget {
 }
 
 class _SearchResultState extends State<SearchResult> {
+
+  final String url = "https://swapi.co/api/people/";
   List data;
+
+  @override
+    void initState() {
+      // TODO: implement initState
+      super.initState();
+      this.getJsonData();
+    }
+
+    Future<String> getJsonData() async{
+      var response = await http.get(
+        // Encode the url
+        Uri.encodeFull(url),
+        // Only accept JSON response
+        headers: {"Accept": "application/json"}
+      );
+
+      print(response.body);
+
+      setState(() {
+        var convertDataToJson = JSON.decode(response.body);
+        data = convertDataToJson['results'];   
+      });
+
+      return "Success";
+    }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: AppBar(
         title: Text(
-          "Search Results"
+          "Retrieve JSON via HTTP Get"
         ),
       ),
-      body: Container(
-        child: Center(
-          child: FutureBuilder(
-            future: DefaultAssetBundle.of(context).loadString('load_json/person.json'),
-            builder: (context,snapshot){
-              // Decode JSON
-              var mydata = JSON.decode(snapshot.data.toString());
-
-              return ListView.builder(
-                itemBuilder: (BuildContext context,int index){
-                  return Card(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: <Widget>[
-                        Text("Name: " + mydata[index]["name"]),
-                        Text("Age: " + mydata[index]["age"]),
-                        Text("Height: " + mydata[index]["height"]),
-                        Text("Gender: " + mydata[index]["gender"]),
-                        Text("Hair Color: " + mydata[index]["hair_color"]),
-                      ],
+      body: ListView.builder(
+        itemCount: data == null ? 0: data.length,
+        itemBuilder: (BuildContext context,int index){
+          return Container(
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Card(
+                    child: Container(
+                      child: Text(data[index]["name"]),
+                      padding: const EdgeInsets.all(20.0),
                     ),
-                  );
-                },
-                itemCount: mydata == null ? 0 : mydata.length,
-              );
-            },
-          ),
-        ),
-      ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),  
     );
   }
 }
